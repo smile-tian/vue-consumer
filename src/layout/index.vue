d00d5cc18f6eb72a1d8c1efbfd7f66a13f98b60d
tianxiang <tianxiang20191013@163.com>
2021-03-23 17:44:14 +0800

    模板

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,33 @@
 <template>
   <el-container>
-    <el-header>Header</el-header>
-    <el-main>Main</el-main>
-    <el-footer>Footer</el-footer>
+    <el-header>
+      <Header/>
+    </el-header>
+    <el-main>
+      <transition name="fade-transform" mode="out-in">
+        <router-view :key="key"/>
+      </transition>
+    </el-main>
+    <el-footer>
+      <Footer/>
+    </el-footer>
   </el-container>
 </template>
 
 <script>
+import {Footer, Header} from './components'
+
 export default {
-name: "index"
+  name: "Layout",
+  components: {
+    Footer,
+    Header
+  },
+  computed: {
+    key() {
+      return this.$route.path
+    }
+  }
 }
 </script>
 
